aff95437148ba510883ff1f30b038baf9b7f205a
Tom Byrne <tom@tbyrne.org>
2022-05-03 21:02:03 +1000

    Added max-depth cutoff for performance

--- src/components/panel/TreeMap.tsx
+++ src/components/panel/TreeMap.tsx
@@ -19,7 +19,7 @@ function TreeMap({ job }: TreeMapProps) {
     dispatch({ type:"jobs/setViewSize", payload:{ job:job.id, width:dimensions.width, height:dimensions.height } })
   }, [dimensions, dispatch, job]);
 
-  function getNodeChildren(node: FileNode) {
+  function getNodeChildren(node: FileNode, maxDepth:number) {
     if (!node.children) return;
     // return node.children?.map((c) => getNode(c));
 
@@ -28,7 +28,7 @@ function TreeMap({ job }: TreeMapProps) {
     for (const child of node.children) {
       if (!child.value) continue;
       nodes.push(
-        getNode(child)
+        getNode(child, maxDepth)
       );
     }
 
@@ -37,17 +37,34 @@ function TreeMap({ job }: TreeMapProps) {
     </div>
   }
 
-  function getNode(node: FileNode, extraClass: string = '') {
+  function formatMb(bytes:number): string{
+    return (bytes / 1000000).toPrecision(2);
+  }
+
+  function getExt(file: string): string {
+      const ind = file.lastIndexOf('.');
+      if (ind === -1) return "";
+      else return file.substring(ind + 1).toLowerCase();
+  }
+
+  function getNode(node: FileNode, maxDepth:number, extraClass: string = '') {
     if (!node.info) return;
-    return <div key={node.info?.path} className={'node ' + node.className + ' ' + extraClass} style={{ left: node.pos_x + 'px', top: node.pos_y + 'px', width: node.pos_w + 'px', height: node.pos_h + 'px' }}>
-      <div className="label" style={{ top: (10 + node.info.depth * 30) + 'px', left: (10 + node.info.depth * 10) + 'px' }}>{`${node.info.depth + 1}. ${node.info.name}`}</div>
-      { getNodeChildren(node) }
+
+    
+    const className = (node.info.is_dir ? "type-dir" : "type-file") +
+                      (!node.info.is_dir ? " ext-" + getExt(node.info.name) : '') +
+                      (` depth-${node.info.depth}`) +
+                      (maxDepth === 0 ? " max-depth" : '');
+
+    return <div key={node.info?.path} className={'node ' + className + ' ' + extraClass} style={{ left: node.pos_x + 'px', top: node.pos_y + 'px', width: node.pos_w + 'px', height: node.pos_h + 'px' }}>
+      <div className="label" style={{ top: (10 + node.info.depth * 30) + 'px', left: (10 + node.info.depth * 10) + 'px' }}>{`${node.info.depth + 1}. ${node.info.name}`} <span>({formatMb(node.info.size)}mb)</span> <span>{!maxDepth ? " - Reached max depth, children omitted" : ''}</span></div>
+      { maxDepth > 0 ? getNodeChildren(node, maxDepth-1) : null }
     </div>
   }
 
   return (
     <div id="tree-cont" ref={treemapRef}>
-      {getNode(job.root, "root-node")}
+      {getNode(job.root, 10, "root-node")}
     </div>
   );
 }
